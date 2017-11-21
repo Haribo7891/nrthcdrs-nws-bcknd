8ca2d24a98ab2efa763a0e071ace6fb1a84d78a9
Haribo7891 <harry.crank@me.com>
2017-11-21 10:06:45 +0000

    Basic setup of Project

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true,
+    "node": true,
+    "jasmine": true,
+    "mocha": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-console": ["warn"],
+    "no-var": ["error"],
+    "no-multi-spaces": ["error"],
+    "no-extra-semi": ["error"],
+    "prefer-const": ["error"],
+    "semi": ["error", "always"],
+    "object-curly-spacing": ["error", "always"],
+    "space-before-blocks": ["error", "always"],
+    "arrow-parens": ["error", "always"],
+    "template-curly-spacing": ["error", "always"],
+    "block-spacing": ["error", "always"],
+    "eol-last": ["error", "always"],
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "quotes": ["error", "single"],
+    "no-multiple-empty-lines": ["error", {
+      "max": 2
+    }],
+    "keyword-spacing": ["error", {
+      "before": true,
+      "after": true
+    }],
+    "arrow-spacing": ["error", {
+      "before": true,
+      "after": true
+    }],
+    "comma-spacing": ["error", {
+      "before": false,
+      "after": true
+    }],
+    "semi-spacing": ["error", {
+      "before": false,
+      "after": true
+    }],
+    "key-spacing": ["error", {
+      "beforeColon": false,
+      "afterColon": true
+    }],
+    "array-bracket-spacing": ["error", "always", {
+      "singleValue": false,
+      "objectsInArrays": false,
+      "arraysInArrays": false
+    }],
+    "space-before-function-paren": ["error", {
+      "anonymous": "always",
+      "named": "always",
+      "asyncArrow": "always"
+    }]
+  }
+};
---
+++ README.md
@@ -0,0 +1,60 @@
+# Northcoders News Project
+
+My reimplementation of the Northcoders News Sprint.
+___
+
+## Setup
+
+To run this project, you will need ```node.js``` (v8.9.1) and ```npm``` (v5.5.1) installed.
+
+1. To check whether you have ```node.js``` installed, run the following code in your terminal:
+
+``` node
+node -v
+```
+
+2. To install ```node.js```, please refer to the following [documentation](https://nodejs.org/en/).
+
+3. To check whether you have ```npm``` installed, run the following code in your terminal:
+
+``` node
+npm -v
+```
+
+4. To install ```npm```, please refer to the following [documentation](https://docs.npmjs.com).
+
+___
+
+## Installation
+
+1. Clone this repo to your local machine:
+
+``` node
+git clone <<GITHUB LINK>>
+```
+
+2. Install the dependencies:
+
+``` node
+npm install
+```
+
+___
+
+## Dependencies
+
+The following dependencies were used in the making of this project:
+
+* [Mocha](https://mochajs.org) - The testing framework.
+* [Chai](http://chaijs.com) - The assertion library.
+
+___
+
+## Author
+
+[Harry Crank](https://github.com/Haribo7891)
+___
+
+## Acknowledgments
+
+Inspired by [Reddit](https://www.reddit.com) and completed as part of a project at [Northcoders](https://northcoders.com/) bootcamp.
